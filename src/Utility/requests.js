a29e69403a28cd6e54217f8d0ee5a85d7761e1c9
da_nielh <daniel37922434@gmail.com>
2024-02-19 03:05:40 +0300

    adding banner

--- src/Utility/requests.js
+++ src/Utility/requests.js
@@ -1,16 +1,16 @@
-
-const API_KEY = process.env.API_KEY
+const API_KEY = '0f7dc1000d1ea098f2fd4885a3189d79';
+// const API_KEY = process.env.REACT_APP_API_KEY;
 
 const requests = {
-    fetchTrending:`/trending/all/week?api_key=${API_KEY}&1anguage=en-US`,
-    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRatedMovies:`/movie/top_rated?api_key=${API_KEY}&1anguage=en-US`,
-    fetchActiojfMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`,
-    fetchTvShow:`tv/popular?api_key=${API_KEY}&1anguage=en-US&page=1`
-}
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`
+};
 
-export default requests
+export default requests;
